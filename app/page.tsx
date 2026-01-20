faf2fa3dfceb97bb6b1903bd0200b67c241d0868
medamchan <chandravardhanreddym.1996@gmail.com>
2026-01-20 14:30:49 -0600

    Change in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js App 🚀</h1>
-      <p>This is a simple Next.js application deploying on EC2 using Jenkins.</p>
+      <p>This is a simple Next.js application.</p>
     </div>
   );
 }
