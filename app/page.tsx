bfb09743316ad98c0c2a9ba777dbe942b4d6c17d
medamchan <chandravardhanreddym.1996@gmail.com>
2026-01-20 14:25:06 -0600

    Change in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to Next.js 🚀</h1>
+      <h1>Welcome to Next.js App 🚀</h1>
       <p>This is a simple Next.js application.</p>
     </div>
   );
