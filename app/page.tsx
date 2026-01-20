cdc3c7865a80b6d8c9bcee6f2e932424f342610c
medamchan <chandravardhanreddym.1996@gmail.com>
2026-01-20 14:44:40 -0600

    Change in app/page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Welcome to Next.js App Built by Chandra🚀</h1>
-      <p>This is a simple Next.js application built by chandra.</p>
+      <p>This is a simple Next.js application.</p>
     </div>
   );
 }
